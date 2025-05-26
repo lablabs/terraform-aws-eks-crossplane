2b743b480f1e14b099f66db5f6a9d9ada91ec2e6
Attila Sovak <attila.sovak@gmail.com>
2025-05-26 16:54:33 +0200

    feat(crossplane): add base uni syntax

---
+++ iam.tf
@@ -0,0 +1,29 @@
+locals {
+  irsa_policy_enabled = var.irsa_policy_enabled != null ? var.irsa_policy_enabled : var.irsa_assume_role_enabled == false
+}
+
+data "aws_iam_policy_document" "this" {
+  count = var.enabled && var.irsa_policy == null && local.irsa_policy_enabled ? 1 : 0
+
+  # Example statement (modify it before using this module)
+  statement {
+    sid = "Autoscaling"
+
+    actions = [
+      "autoscaling:DescribeAutoScalingGroups",
+      "autoscaling:DescribeAutoScalingInstances",
+      "autoscaling:DescribeLaunchConfigurations",
+      "autoscaling:DescribeTags",
+      "autoscaling:SetDesiredCapacity",
+      "autoscaling:TerminateInstanceInAutoScalingGroup",
+      "ec2:DescribeLaunchTemplateVersions",
+      "ec2:DescribeInstanceTypes"
+    ] # checkov:skip=CKV_AWS_111
+
+    resources = [
+      "*",
+    ]
+
+    effect = "Allow"
+  }
+}
--- main.tf
+++ main.tf
@@ -1,31 +1,28 @@
 /**
- * # AWS EKS Universal Addon Terraform module
+ * # AWS EKS Crossplane Terraform module
  *
- * A Terraform module to deploy the universal addon on Amazon EKS cluster.
+ * A Terraform module to deploy the [Crossplane](https://www.crossplane.io/) on Amazon EKS cluster.
  *
- * [![Terraform validate](https://github.com/lablabs/terraform-aws-eks-universal-addon/actions/workflows/validate.yaml/badge.svg)](https://github.com/lablabs/terraform-aws-eks-universal-addon/actions/workflows/validate.yaml)
- * [![pre-commit](https://github.com/lablabs/terraform-aws-eks-universal-addon/actions/workflows/pre-commit.yaml/badge.svg)](https://github.com/lablabs/terraform-aws-eks-universal-addon/actions/workflows/pre-commit.yaml)
+ * [![Terraform validate](https://github.com/lablabs/terraform-aws-eks-crossplane/actions/workflows/validate.yaml/badge.svg)](https://github.com/lablabs/terraform-aws-eks-crossplane/actions/workflows/validate.yaml)
+ * [![pre-commit](https://github.com/lablabs/terraform-aws-crossplane/actions/workflows/pre-commit.yml/badge.svg)](https://github.com/lablabs/terraform-aws-eks-crossplane/actions/workflows/pre-commit.yml)
 */
 
 locals {
   addon = {
-    name = "universal-addon"
+    name = "crossplane"
 
-    helm_chart_name    = "raw"
-    helm_chart_version = "0.1.0"
-    helm_repo_url      = "https://lablabs.github.io"
+    helm_chart_version = "1.20.0-rc.0.140.g629ca2e2f"
+    helm_repo_url      = "https://charts.crossplane.io/master/"
   }
 
   addon_irsa = {
     (local.addon.name) = {
+      irsa_policy_enabled = local.irsa_policy_enabled
+      irsa_policy         = var.irsa_policy != null ? var.irsa_policy : try(data.aws_iam_policy_document.this[0].json, "")
     }
   }
 
-  addon_values = yamlencode({
-    # FIXME config: add default values here or leave empty if not needed
-  })
+  addon_values = yamlencode({})
 
-  addon_depends_on = [
-    # FIXME config: add dependencies here, i.e. CRDs, or leave empty if not needed
-  ]
+  addon_depends_on = []
 }
